6f93afd10b054730489efdc8fb99fdcd8cfd4caa
dokuritsu17 <dokuritsu17@gmail.com>
2020-01-24 14:38:26 -0600

    Last minute corrections; Added word doc with pics

--- Lab1/AccountHolderTest.java
+++ Lab1/AccountHolderTest.java
@@ -10,8 +10,8 @@ public class AccountHolderTest
 {
     public static void main(final String[] args) {
 
-        // Initially the interest for the bank will be set to 4%
 
+        // Initially the interest for the bank will be set to 4%
         // Ask the user to for the initial account balance
         System.out.println("Please provide the initial account balance. Note: The annual interest rate is 4%.");
         Scanner scan = new Scanner(System.in);
@@ -36,11 +36,13 @@ public class AccountHolderTest
         ah1.monthlyInterest();
 
         // Inform the user of the final balance
-        final String finalBal = String.format("Ending balance for the month including monthly interest: %.2f",
+
+        
+        String finalBal = String.format("Ending balance for the month including monthly interest: %.2f",
                 ah1.getBalance());
         System.out.println(finalBal);
-        
-        //Close the scanner
+
+        // Close the scanner
         scan.close();
 
     }
