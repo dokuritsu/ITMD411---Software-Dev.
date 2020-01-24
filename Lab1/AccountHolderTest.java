17663d6775f5f53143c60e5fb6d60175f7ed51e0
dokuritsu17 <dokuritsu17@gmail.com>
2020-01-24 13:28:25 -0600

    Some minor changes to the state of the variables

--- Lab1/AccountHolderTest.java
+++ Lab1/AccountHolderTest.java
@@ -4,37 +4,40 @@ import java.util.Scanner;
 
 public class AccountHolderTest
 {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
 
-        //Initially the interest for the bank will be set to 4%
+        // Initially the interest for the bank will be set to 4%
 
-        //Ask the user to for the initial account balance
+        // Ask the user to for the initial account balance
         System.out.println("Please provide the initial account balance. Note: The annual interest rate is 4%.");
-        Scanner scan = new Scanner(System.in);
-        double initialBal = scan.nextDouble();
+        final Scanner scan = new Scanner(System.in);
+        final double initialBal = scan.nextDouble();
 
-        //Create object with provided initial balance
-        AccountHolder ah1 = new AccountHolder(initialBal);
+        // Create object with provided initial balance
+        final AccountHolder ah1 = new AccountHolder(initialBal);
 
-        //Ask the user to make a deposit
+        // Ask the user to make a deposit
         System.out.println("Please provide an amount to deposite into account: ");
 
-        //Do the deposit
+        // Do the deposit
         ah1.deposit(scan.nextDouble());
 
-        //Ask the user to make a withdrawal
+        // Ask the user to make a withdrawal
         System.out.println("Please provide an amount to withdraw from account:");
-        
-        //Withdraw
+
+        // Withdraw
         ah1.withdrawal(scan.nextDouble());
 
-        //Calculate the balance at the end of month
+        // Calculate the balance at the end of month
         ah1.monthlyInterest();
 
-        //Inform the user of the final balance
-        String finalBal = String.format("Ending balance for the month including monthly interest: %.2f", ah1.getBalance());
+        // Inform the user of the final balance
+        final String finalBal = String.format("Ending balance for the month including monthly interest: %.2f",
+                ah1.getBalance());
         System.out.println(finalBal);
         
+        //Close the scanner
+        scan.close();
 
     }
 }
