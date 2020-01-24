f8dd46b24d2df295694edcc48926170c6dcff536
dokuritsu17 <dokuritsu17@gmail.com>
2020-01-24 13:08:50 -0600

    Set the default interest rate and more

--- Lab1/AccountHolderTest.java
+++ Lab1/AccountHolderTest.java
@@ -5,10 +5,16 @@ import java.util.Scanner;
 public class AccountHolderTest
 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        double balance = 0;
 
-        //Create object
-        AccountHolder ah1 = new AccountHolder();
+        //Initially the interest for the bank will be set to 4%
+        double interestRate = 0.04;
+
+        //Ask the user to for the initial account balance
+        System.out.println("Please provide the initial account balance. Note: The annual interest rate is 4%.");
+        Scanner scan = new Scanner(System.in);
+        double initialBal = scan.nextDouble();
+
+        //Create object with provided initial balance
+        AccountHolder ah1 = new AccountHolder(initialBal);
     }
 }
