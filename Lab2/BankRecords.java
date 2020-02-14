76b75bcf5b613a8583cfe887f0d5a1ff5c31d851
Laura Pereda <lypereda17@gmail.com>
2020-02-14 11:09:54 -0600

    created BankRecords and its fields

---
+++ Lab2/BankRecords.java
@@ -0,0 +1,14 @@
+public class BankRecords{
+    private String id;
+    private int age;
+    private String sex;
+    private String region;
+    private int income;
+    private String martialStatus;
+    private int totalChildren;
+    private String hasCar;
+    private String save_act;
+    private String current_act;
+    private String mortgage;
+    private String pep;
+}
