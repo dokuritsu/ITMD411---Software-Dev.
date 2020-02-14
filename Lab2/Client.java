49c150c9c58635b660fc4c6ba77e2fe4d62eec06
Laura Pereda <lypereda17@gmail.com>
2020-02-14 10:57:15 -0600

    Created abstract client class and its methods

---
+++ Lab2/Client.java
@@ -0,0 +1,11 @@
+public abstract class Client{
+
+    //First method: readData()
+    public abstract void readData();
+
+    //Second method: processData()
+    public abstract void processData();
+
+    //Third method: printData();
+    public abstract void printData();
+}
