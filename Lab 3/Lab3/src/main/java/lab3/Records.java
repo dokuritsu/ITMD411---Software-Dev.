64374b0f8eefd9695b5d68ee8f3128736edd436a
Laura Pereda <lypereda17@gmail.com>
2020-03-24 19:16:00 -0500

    completed count of males & females

--- Lab 3/Lab3/src/main/java/lab3/Records.java
+++ Lab 3/Lab3/src/main/java/lab3/Records.java
@@ -23,13 +23,14 @@ public class Records extends BankRecords {
 
         //See the first 10 unsorted items
         System.out.println("Unsorted");
-        for(int i = 0; i < records.getbArrayList().size(); i++){
+        for(int i = 0; i < 10; i++){
             System.out.println(records.getbArrayList().get(i));
         }
 
         //See the first 10 sorted items
+        System.out.println("Sorted");
         AvgIncome(records.getbArrayList());
-        for(int i = 0; i < records.getbArrayList().size(); i++){
+        for(int i = 0; i < 10; i++){
             System.out.println(records.getbArrayList().get(i));
         }
 
@@ -47,6 +48,37 @@ public class Records extends BankRecords {
      * Method to determine average income by sec
      */
     public static void AvgIncome(ArrayList<BankRecords> arrayList){
+
+        // Sort the data by males vs females...
         Collections.sort(arrayList, new SexComparator());
+
+        /*
+        *   To keep track of:
+        *   Counts for # of males & females
+        *   Total income from males & females
+        */
+        int mCount, fCount;
+        double totalMIncome, totalFIncome;
+        mCount = fCount = 0;
+        totalFIncome = totalMIncome = 0;
+
+        for (int i = 0; i < arrayList.size(); i++){
+            if (arrayList.get(i).getSex().equals("FEMALE")){
+                // Increment fCount
+                fCount++;
+
+                // Add income to totalFIncome
+                totalFIncome += arrayList.get(i).getIncome();
+            } else {
+                // Increment mCount
+                mCount++;
+
+                // Add income to totalMIncome
+                totalMIncome += arrayList.get(i).getIncome();
+            }
+        }
+
+        System.out.println("Total # of Females: " + fCount);
+        System.out.println("Total income from females: " + totalFIncome);
     }
 }
