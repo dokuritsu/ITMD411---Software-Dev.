c01e0da69c47fbb52d4f35dcd1740beb053fc07b
Laura Pereda <lypereda17@gmail.com>
2020-03-27 11:55:40 -0500

    remove todo statement

--- Lab 3/Lab3/src/main/java/lab3/SexComparator.java
+++ Lab 3/Lab3/src/main/java/lab3/SexComparator.java
@@ -6,7 +6,6 @@ public class SexComparator implements Comparator<BankRecords> {
 
     @Override
     public int compare(BankRecords o1, BankRecords o2) {
-        // TODO Auto-generated method stub
         int result = o1.getSex().compareTo(o2.getSex());
         return result;
     }
