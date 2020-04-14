f0da26cc14d76ccf286db4837f29dd8ce5e00cb2
Laura Pereda <lypereda17@gmail.com>
2020-04-14 16:44:55 -0500

    testing gui with data

--- Lab 4/lab4/src/main/java/lab4/LoanProcessing.java
+++ Lab 4/lab4/src/main/java/lab4/LoanProcessing.java
@@ -3,27 +3,40 @@ package lab4;
 // Imports
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.swing.JFrame; 
+import javax.swing.JScrollPane; 
+import javax.swing.JTable; 
 
 public class LoanProcessing extends BankRecords{
+
+    static JFrame f;
+    static JTable j;
+    static ArrayList<String[]> records = new ArrayList<>();
 
     public static void main(String[] args) {
         // Object instantiations
         BankRecords br = new BankRecords();
         Dao dao = new Dao();
+        f = new JFrame();
+
+        // Set the Frame Title
+        f.setTitle("Loan Analysis");
 
         // Read data from csv file...
         br.readData();
 
         // Create a table; Comment out after running once
-        dao.createTables();
+        //dao.createTables();
 
         // Insert the records
-        dao.insertRecords(br.getbArrayList());
+        //dao.insertRecords(br.getbArrayList());
 
         // Fill the result set object
         ResultSet rs = dao.retrieveRecords();
 
-        System.out.println("ID:\t\tINCOME:\t\t\t PEP:");
+        System.out.println("ID:\t\tINCOME:\t\t\tPEP:");
 
         // Extracting data from result set
         try {
@@ -33,6 +46,10 @@ public class LoanProcessing extends BankRecords{
                 String income = rs.getString(2);
                 String pep = rs.getString(3);
 
+                // Create a 1D array of the records & add to arraylist for easy access
+                String[] curr = {removeID, income, pep};
+                records.add(curr);
+                
                 String printout = String.format("%s\t\t%s \t\t%s", removeID, income, pep);
                 System.out.println(printout);
             }
@@ -42,5 +59,35 @@ public class LoanProcessing extends BankRecords{
         } catch (SQLException se) {
             se.printStackTrace();
         }
+
+        showTable(records);
+
+    }
+
+    public static void showTable(ArrayList<String[]> records) {
+        // Column names for table
+        String [] columnNames = {"ID", "INCOME", "PEP"};
+
+        // Since we know the amount of records & # of columns...
+        String[][] data = new String[600][3];
+
+        // Add the records into the 2D array
+        for (int i = 0; i < records.size(); i++) {
+            data[i] = records.get(i);
+        }
+
+        // Create table with data & column name
+        j = new JTable(data, columnNames);
+        j.setBounds(30, 40, 200, 300);
+
+        // Adding it to JScrollPane
+        JScrollPane sp = new JScrollPane(j);
+        f.add(sp);
+
+        // Set the frame size
+        f.setSize(500, 200);
+
+        // Set the frame as visible
+        f.setVisible(true);
     }
 }
