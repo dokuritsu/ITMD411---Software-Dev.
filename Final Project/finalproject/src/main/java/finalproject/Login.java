25b42ad947b79bc2e7cb41db41fbeeb81665313c
Laura Pereda <lypereda17@gmail.com>
2020-05-08 16:48:35 -0500

    found a decent color

--- Final Project/finalproject/src/main/java/finalproject/Login.java
+++ Final Project/finalproject/src/main/java/finalproject/Login.java
@@ -32,16 +32,16 @@ public class Login extends JFrame{
         setLocationRelativeTo(null); // centers window
 
         // Add background color
-        Color c = new Color(148,0,211);
+        Color c = new Color(147,112,219);
         getContentPane().setBackground(c);
 
         // Set up a border
         ((JComponent) getContentPane()).setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.white), BorderFactory.createCompoundBorder(BorderFactory.createRaisedBevelBorder(), BorderFactory.createLoweredBevelBorder())));
 
-        JLabel title = new JLabel("IIT HELP", JLabel.RIGHT);
+        JLabel title = new JLabel("IIT HELP ", JLabel.RIGHT);
         JLabel title1 = new JLabel("DESK LOGIN", JLabel.LEFT);
-        title.setBackground(Color.black);
-        title1.setBackground(Color.black);
+        title.setForeground(Color.white);
+        title1.setForeground(Color.white);
         
         // Set up controls
         JLabel lblUsername = new JLabel("Username", JLabel.LEFT);
@@ -59,8 +59,15 @@ public class Login extends JFrame{
 
         
         // Buttons for logging and exiting
-		JButton btn = new JButton("Submit");
+		JButton btn = new JButton("Login");
         JButton btnExit = new JButton("Exit");
+
+        // Add color
+        Color c2 = new Color(75,0,130);
+        btnExit.setBackground(c2);
+        btn.setBackground(c2);
+        btnExit.setForeground(Color.white);
+        btn.setForeground(Color.white);
         
         // Constraints
         lblStatus.setToolTipText("Contact help desk to unlock password");
@@ -68,13 +75,15 @@ public class Login extends JFrame{
         lblPassword.setHorizontalAlignment(JLabel.CENTER);
         
         // Add objects to frame
+        add(title);
+        add(title1);
         add(lblUsername);  // 1st row filler
 		add(txtUname);
 		add(lblPassword); // 2nd row
 		add(txtPassword);
 		add(btn);         // 3rd row
 		add(btnExit);
-        add(lblStatus);   // 4th row
+        //add(lblStatus);   // 4th row
         
         setVisible(true); // SHOW THE FRAME
     }
