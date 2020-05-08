845f9092e6770e2e67551537b89ef3824a86a0b1
Laura Pereda <lypereda17@gmail.com>
2020-05-08 16:52:31 -0500

    Set up action listeners for both buttons

--- Final Project/finalproject/src/main/java/finalproject/Login.java
+++ Final Project/finalproject/src/main/java/finalproject/Login.java
@@ -1,6 +1,8 @@
 package main.java.finalproject;
 
 import java.awt.GridLayout; //useful for layouts
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -12,8 +14,8 @@ import javax.swing.JTextField;
 
 import java.awt.Color;
 
-public class Login extends JFrame{
-    
+public class Login extends JFrame {
+
     // Establish a connection
     Dao connect;
 
@@ -24,7 +26,7 @@ public class Login extends JFrame{
 
         // Create the tables in the database (Remove later on)
         connect = new Dao();
-        //connect.createTables();
+        // connect.createTables();
 
         // Set up some basics
         setSize(400, 210);
@@ -32,21 +34,24 @@ public class Login extends JFrame{
         setLocationRelativeTo(null); // centers window
 
         // Add background color
-        Color c = new Color(147,112,219);
+        Color c = new Color(147, 112, 219);
         getContentPane().setBackground(c);
 
         // Set up a border
-        ((JComponent) getContentPane()).setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.white), BorderFactory.createCompoundBorder(BorderFactory.createRaisedBevelBorder(), BorderFactory.createLoweredBevelBorder())));
+        ((JComponent) getContentPane())
+                .setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.white),
+                        BorderFactory.createCompoundBorder(BorderFactory.createRaisedBevelBorder(),
+                                BorderFactory.createLoweredBevelBorder())));
 
         JLabel title = new JLabel("IIT HELP ", JLabel.RIGHT);
         JLabel title1 = new JLabel("DESK LOGIN", JLabel.LEFT);
         title.setForeground(Color.white);
         title1.setForeground(Color.white);
-        
+
         // Set up controls
         JLabel lblUsername = new JLabel("Username", JLabel.LEFT);
-		JLabel lblPassword = new JLabel("Password", JLabel.LEFT);
-		JLabel lblStatus = new JLabel(" ", JLabel.CENTER);
+        JLabel lblPassword = new JLabel("Password", JLabel.LEFT);
+        JLabel lblStatus = new JLabel(" ", JLabel.CENTER);
         // JLabel lblSpacer = new JLabel(" ", JLabel.CENTER);
 
         // Add color
@@ -57,33 +62,46 @@ public class Login extends JFrame{
         JTextField txtUname = new JTextField(10);
         JPasswordField txtPassword = new JPasswordField();
 
-        
         // Buttons for logging and exiting
-		JButton btn = new JButton("Login");
+        JButton btn = new JButton("Login");
         JButton btnExit = new JButton("Exit");
 
         // Add color
-        Color c2 = new Color(75,0,130);
+        Color c2 = new Color(75, 0, 130);
         btnExit.setBackground(c2);
         btn.setBackground(c2);
         btnExit.setForeground(Color.white);
         btn.setForeground(Color.white);
-        
+
         // Constraints
         lblStatus.setToolTipText("Contact help desk to unlock password");
-		lblUsername.setHorizontalAlignment(JLabel.CENTER);
+        lblUsername.setHorizontalAlignment(JLabel.CENTER);
         lblPassword.setHorizontalAlignment(JLabel.CENTER);
-        
+
         // Add objects to frame
         add(title);
         add(title1);
-        add(lblUsername);  // 1st row filler
-		add(txtUname);
-		add(lblPassword); // 2nd row
-		add(txtPassword);
-		add(btn);         // 3rd row
-		add(btnExit);
-        //add(lblStatus);   // 4th row
+        add(lblUsername); // 1st row filler
+        add(txtUname);
+        add(lblPassword); // 2nd row
+        add(txtPassword);
+        add(btn); // 3rd row
+        add(btnExit);
+        // add(lblStatus); // 4th row
+
+        // Add the action listeners
+        btn.addActionListener(new ActionListener() {
+            int count = 0; // count agent
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Login button clicked");
+
+            }
+            
+        });
+
+        btnExit.addActionListener(e -> System.exit(0));
         
         setVisible(true); // SHOW THE FRAME
     }
