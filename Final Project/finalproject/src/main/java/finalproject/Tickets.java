7cf3709368568bf5308852d87c7b4c70ef77d350
Laura Pereda <lypereda17@gmail.com>
2020-05-08 17:11:40 -0500

    begin ticket frame

--- Final Project/finalproject/src/main/java/finalproject/Tickets.java
+++ Final Project/finalproject/src/main/java/finalproject/Tickets.java
@@ -1,5 +1,35 @@
 package main.java.finalproject;
 
-public class Tickets {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+
+import javax.swing.JFrame;
+
+public class Tickets extends JFrame implements ActionListener{
+
+    // For connecting to db
+    Dao dao = new Dao(); // for CRUD operations
+    Boolean chkIfAdmin = null;
+
+    // Limit what is shown depending on who logs in
+    public Tickets(Boolean isAdmin) {
+        if (chkIfAdmin != isAdmin){
+            try{
+
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+		createMenu();
+		prepareGUI();
+
+	}
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+
+    }
     
 }
